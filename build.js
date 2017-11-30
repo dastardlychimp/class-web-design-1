4c2744dee08705fc781608a5dae8ce0f7d94d0fa
dastardlychimp <darien.hess@demochimp.com>
2017-11-30 16:25:06 -0700

    [module-3] Update to now use module name as directory.

--- build.js
+++ build.js
@@ -1,12 +1,17 @@
 const path     = require('path')
 const fs       = require('fs')
 const pug      = require('pug')
+const del      = require('del')
+const config   = require('./config')
 const glossary = require('./glossary')
 
-const path_page       = path.join(__dirname, '/templates/pages')
-const path_build      = path.join(__dirname, '/build')
-const path_build_html = path.join(path_build, '/pages')
+const path_build        = path.join(__dirname, '/build')
+const path_build_module = path.join(path_build, config.MODULE)
+const path_build_html   = path.join(path_build_module, '/')
+const path_page         = path.join(__dirname, '/templates/pages')
+
 const data = {
+    base_path: path.join('/', config.MODULE),
     glossary,
     pages: {
         'index': 'About',
@@ -17,28 +22,47 @@ const data = {
     }
 }
 
-fs.readdir(path_page, (err, files) => {
-    if (err) throw err
+del([`${path_build}/*`])
+    .then(makeDirPromise(path_build_module))
+    .then(writeHtmlFiles)
 
-    const promises = files.map((file) => {
-        const path_file = path.join(path_page, file)
-        const path_file_write = path.format({
-            dir: path_build_html,
-            name: path.basename(file, '.pug'),
-            ext: '.html'
-        })
-        const html = pug.renderFile(path_file, data)
+function writeHtmlFiles() {
+    return new Promise((resolve, reject) => {
+        fs.readdir(path_page, (err, files) => {
+            if (err) throw err
+    
+            const promises = files.map((file) => {
+                const path_file = path.join(path_page, file)
+                const path_file_write = path.format({
+                    dir: path_build_html,
+                    name: path.basename(file, '.pug'),
+                    ext: '.html'
+                })
+                const html = pug.renderFile(path_file, data)
+    
+                return writeFilePromise(path_file_write, html)
+                    .catch(e => console.log(e))
+            })
 
-        return writeFilePromise(path_file_write, html)
-            .catch(e => console.log(e))
+            Promise.all(promises).then(resolve)
         })
     })
-    
-    function writeFilePromise(path, content) {
-        return new Promise((resolve, reject) => {
-            fs.writeFile(path, content, err => {
-                if (err) reject(err)
-                else resolve()
-            })
+}
+
+function makeDirPromise(path) {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(path, err => {
+            if (err) throw err
+            else resolve()
+        })
+    })
+}
+
+function writeFilePromise(path, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, content, err => {
+            if (err) throw err
+            else resolve()
+        })
     })
 }
---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MODULE: 'module-3',
+}
