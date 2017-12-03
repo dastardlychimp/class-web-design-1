67f1c74117085802dc1e0f9d40232254533defa5
dastardlychimp <darien.hess@demochimp.com>
2017-12-02 19:38:31 -0700

    [module-4] fixed issue with replacing glossary terms of multi worded terms

--- scripts/replace_glossary_terms.js
+++ scripts/replace_glossary_terms.js
@@ -3,12 +3,28 @@ import term_template from '../templates/functions/compiled/term'
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     const p_tags = Array.from(document.getElementsByTagName('p'))
+    const storage = {}
     
-    Object.keys(glossary).forEach(term => {
-        const linked_term = term_template({ term, key: term.split(' ').join('-') })
-        const termRegex = new RegExp('\\b' + term + '\\b', 'gi')
-        console.log(termRegex)
-        console.log(linked_term)
-        p_tags.forEach(p => p.innerHTML = p.innerHTML.replace(termRegex, linked_term))
-    })
+    Object.keys(glossary)
+        .sort((a, b) => a.length < b.length ? 1 : -1)
+        .forEach(term => {
+            console.log(term)
+            const termRegex = new RegExp('\\b(' + term + ')\\b', 'gi')
+            p_tags.forEach(p => p.innerHTML = p.innerHTML.replace(
+                termRegex,
+                (match, termCaseSensitive) => { 
+                    let id = Math.random()
+                    storage[id] = term_template({
+                        key: term.split(' ').join('-'),
+                        term: termCaseSensitive
+                    })
+                    return id;
+                }
+            ))
+        })
+    
+    p_tags.forEach(p => Object.keys(storage).forEach(id => {
+        console.log(id, storage[id])
+        return p.innerHTML = p.innerHTML.replace(id, storage[id])
+    }))
 })
