f1bf79ffc7dd995eaa58bc53966b2ae2c72f4356
dastardlychimp <darien.hess@demochimp.com>
2017-12-05 10:51:03 -0700

    [module-5] Added my own image to home page.

--- scripts/lazy_load_images.js
+++ scripts/lazy_load_images.js
@@ -1,7 +1,8 @@
-// import echo from './echo.min.js'
 const echo = require('./echo.min.js')(document)
 
 echo.init({
     offsetVertical: 1000,
     throttle: 50
 })
+
+window.setTimeout(echo.render, 0)
