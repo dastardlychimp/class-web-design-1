dc311efbc9ec7805856ba39a445d8945933ce468
dastardlychimp <darien.hess@demochimp.com>
2017-12-05 11:15:36 -0700

    [module-5] have term template function receive the correct module.

--- template_functions.js
+++ template_functions.js
@@ -1,8 +1,20 @@
 const fs = require('fs')
+const path = require('path')
 const pug = require('pug')
+const config = require('./config')
+
+const compiledPath = './templates/functions/compiled';
 
 const templates = {
-    'term': 'a.term(href=`/module-4/glossary.html#${key}`)= term'
+    'term': `a.term(href=\`/${config.MODULE}/glossary.html#\$\{key\}\`)= term`
 }
 
-fs.writeFileSync('./templates/functions/compiled/term.js', pug.compileClient(templates.term, {name: 'term'}))
+Object.keys(templates).forEach((name) => {
+    const template_path = path.join(compiledPath, name + '.js')
+    const opts = {
+        name
+    }
+
+    fs.writeFileSync(template_path, pug.compileClient(templates.term, opts))
+    fs.appendFileSync(template_path, `module.exports = ${name};`)
+})
